a7c84ad82ec31c16f5a791f9a82957838ef3d649
Godgift144 <152287872+Godgift144@users.noreply.github.com>
2024-05-18 23:31:04 +0000

    task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,3 +1,9 @@
 function processArray(arr) {
     return arr.map(num => num % 2 === 0 ? num ** 2 : num * 3);
 }
+
+function formatArrayStrings(strings, numbers) {
+    return strings.map((str, index) => {
+        return numbers[index] % 2 === 0 ? str.toUpperCase() : str.toLowerCase();
+    });
+}
